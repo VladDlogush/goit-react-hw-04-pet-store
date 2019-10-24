dea4b53b82c7fb21832150c93837abb5a5443304
VladDlogush <vlad.dlogush21@gmail.com>
2019-10-24 16:54:05 +0300

    add React Loadable

--- my-app/src/components/App/App.jsx
+++ my-app/src/components/App/App.jsx
@@ -1,21 +1,50 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Loadable from 'react-loadable';
 import Nav from '../Nav/Nav';
-import HomePage from '../../pages/Home/Home';
-import PetPage from '../../pages/Pet/Pet';
-import PetsPage from '../../pages/Pets/Pets';
-import AboutPage from '../../pages/About/About';
-import NotFoundPage from '../../pages/NotFound/NotFound';
+import Loader from '../Loader/Loader';
+
+const AsyncHomePage = Loadable({
+  loader: () =>
+    import('../../pages/Home/Home' /* webpackChunkName: "home-page" */),
+  loading: Loader,
+});
+
+const AsyncPetPage = Loadable({
+  loader: () =>
+    import('../../pages/Pet/Pet' /* webpackChunkName: "pet-page" */),
+  loading: Loader,
+});
+
+const AsyncPetsPage = Loadable({
+  loader: () =>
+    import('../../pages/Pets/Pets' /* webpackChunkName: "pets-page" */),
+  loading: Loader,
+});
+
+const AsyncAboutPage = Loadable({
+  loader: () =>
+    import('../../pages/About/About' /* webpackChunkName: "about-page" */),
+  loading: Loader,
+});
+
+const AsyncNotFoundPage = Loadable({
+  loader: () =>
+    import(
+      '../../pages/NotFound/NotFound' /* webpackChunkName: "not-found-page" */
+    ),
+  loading: Loader,
+});
 
 const App = () => (
   <div>
     <Nav />
     <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/pets/:id" component={PetPage} />
-      <Route path="/pets" component={PetsPage} />
-      <Route path="/about" component={AboutPage} />
-      <Route component={NotFoundPage} />
+      <Route path="/" exact component={AsyncHomePage} />
+      <Route path="/pets/:id" component={AsyncPetPage} />
+      <Route path="/pets" component={AsyncPetsPage} />
+      <Route path="/about" component={AsyncAboutPage} />
+      <Route component={AsyncNotFoundPage} />
     </Switch>
   </div>
 );
